b61ecde1da14275ef97a48ddd226b7c8ffd60d6a
hongrongjie <hongrongjie@fusion.gat.com>
2019-01-18 12:16:46 -0800

    plot shear flow profiles

--- plot_v_perp.py
+++ plot_v_perp.py
@@ -24,7 +24,7 @@ def read_idl(shot_number: int) -> object:
     return idl_data
 
 
-def plot_spec(shots):
+def plot_vel(shots):
     fig, axs = plt.subplots(1, 1, figsize=[4, 4])
 
     for shot in shots:
@@ -44,34 +44,35 @@ def plot_spec(shots):
 
         # interpolate genray data to fit dbs data
         k_time = np.zeros((8, len(time)))
-        # psi_time = np.zeros_like(k_time)
+        psi_time = np.zeros_like(k_time)
         for i in range(8):
             foo_intp = interp1d(t_sc, k_sc[:, i], kind='nearest')
             k_time[i, :] = foo_intp(time)
-            # foo_intp = interp1d(t_sc, psi_sc[:, i], kind='nearest')
-            # psi_time[i, :] = foo_intp(time)
+            foo_intp = interp1d(t_sc, psi_sc[:, i], kind='nearest')
+            psi_time[i, :] = foo_intp(time)
 
-        # integrate quadrature wrt frequency for density fluctuation level
-        # get time mean and std values for each channel of density and k_scatter
-        Sm = np.sum(Sx, axis=1)
-        ne_m = Sm.mean(axis=1)
-        ne_err = Sm.std(axis=1) / np.sqrt(Sm.shape[1] - 1)
-        # k_m = k_sc.mean(axis=0)
-        # k_err = k_sc.mean(axis=0) / np.sqrt(k_sc.shape[0] - 1)
-        k_m = k_time.mean(axis=1)
-        k_err = k_time.std(axis=1) / np.sqrt(k_time.shape[1] - 1)
+        f_av = np.sum(Sx * freq[None, :, None], axis=1) / np.sum(Sx, axis=1)
+        vel_perp = 2 * np.pi * f_av / k_time / 1e2
 
-        axs.errorbar(k_m, ne_m, yerr=ne_err, xerr=k_err, fmt='o',
-                     label=f'#{shot}')
-        axs.set(yscale='log', xscale='log')
+        # get time mean and std values for each channel
+        vel_perp_m = vel_perp.mean(axis=1)
+        vel_perp_err = vel_perp.std(axis=1) / np.sqrt(vel_perp.shape[1] - 1)
+        psi_m = psi_time.mean(axis=1)
+        psi_err = psi_time.std(axis=1) / np.sqrt(psi_time.shape[1] - 1)
 
-    axs.set_xlabel(r'$k_\perp$ (rad/cm)')
-    axs.set_title(f"k-spectra")
-    axs.set(ylim=[5e-4, 1e-1], xlim=[4, 30])
+        # for i in range(8):
+        #     axs.plot(psi_time[i, :], vel_perp[i, :], '.', c='gray', alpha=0.1)
+        axs.errorbar(psi_m, vel_perp_m, yerr=vel_perp_err, xerr=psi_err,
+                     fmt='o-', label=f'#{shot}')
+
+    axs.set_xlabel(r'$\psi_n$')
+    axs.set_title(r"$V_\perp$ (km/s)")
+    axs.set(ylim=[-8, 1], xlim=[0, 1])
     axs.legend(loc=1, fontsize=9)
     sns.despine()
     plt.tight_layout()
-    plt.savefig(f'../fig/k_spec_{shots[0]}_{shots[-1]}.pdf', transparent=True)
+    plt.savefig(f'../fig/vel_perp_{shots[0]}_{shots[-1]}.pdf',
+                transparent=True)
     plt.show()
 
 
@@ -82,5 +83,7 @@ if __name__ == "__main__":
     shots1 = [171956, 171957, 171958, 171959]
     shots2 = [171963, 171964, 171965]
     shots3 = [171967, 171968, 171969]
-    for shots in [shots1, shots2, shots3]:
-        plot_spec(shots)
+    shots_vec = [shots1, shots2, shots3]
+    # shots_vec = [[171968]]
+    for shots in shots_vec:
+        plot_vel(shots)
